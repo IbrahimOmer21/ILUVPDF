c45be25e8b25630f3cecb47fcc920e729bbe4822
IbrahimOmer21 <ibrahimomer.uni@gmail.com>
2026-03-15 20:49:11 +0200

    merger route

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import render_template, Flask
+from flask import Flask,render_template, Flask, request, redirect, url_for
 
 app= Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route('/merge',methods=['POST'])
+def upload_files():
+    uploaded_files=request.files.getlist['file']
+    if uploaded_files.filename !="":
+        uploaded_files.save(uploaded_files.filename)
 
 
 if __name__=="__main__":
