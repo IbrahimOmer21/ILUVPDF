e5f4586da5b416727957b8ec46938da52db63de4
IbrahimOmer21 <ibrahimomer.uni@gmail.com>
2026-03-19 12:01:48 +0200

    Merging PDFs Complete

--- app.py
+++ app.py
@@ -1,17 +1,41 @@
-from flask import Flask,render_template, Flask, request, redirect, url_for
+from flask import send_file,Flask,render_template, Flask, request, redirect, url_for
+import PyPDF2 as pydf
+import os
+
+import pdf2docx
 
 app= Flask(__name__)
+
+app.config['UPLOAD_FOLDER']='uploads'
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 
-@app.route('/merge',methods=['POST'])
+@app.route('/merge',methods=['GET','POST'])
 def upload_files():
-    uploaded_files=request.files.getlist['file']
-    if uploaded_files.filename !="":
-        uploaded_files.save(uploaded_files.filename)
+    uploaded_files=request.files.getlist('file')
+    
+    merger = pydf.PdfMerger()
+
+
+    for pdfs in uploaded_files:
+        if pdfs.filename !="":
+            path=os.path.join(app.config['UPLOAD_FOLDER'],pdfs.filename)
+            pdfs.save(path)
+            print("saved",path)
+            print("file exists",os.path.exists(path))
+            merger.append(path)
+
+    print("writing merged pdf")
+    output_path = "Merged.pdf"
+    merger.write(output_path)
+    print('done')
+    merger.close() 
+
+    return send_file(output_path, as_attachment=True)
+
 
 
 if __name__=="__main__":
